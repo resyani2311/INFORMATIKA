8a121f089b9a12ffcb4109d6a7898a43b0a5a6fa
Resyani <ecaresya23@gmail.com>
2025-05-15 20:45:23 +0700

    TUGAS KALKULATOR

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+na = 20
+nb = 60
+operatore = '+'
+
+if operatore == '+':
+    hasil_resya = na + nb
+elif operatore == '-':
+    hasil_resya = na - nb
+elif operatore == '*':
+    hasil_resya = na * nb
+elif operatore == '/':
+    hasil_resya = na / nb
+else:
+    print('Data yang Anda masukkan salah')
+
+print("Hasil:", hasil_resya)
